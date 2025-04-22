68a14250abed773aeaf74ef2aac2dc970ba3c9b4
Nick <nicolasgomezaragon@proton.me>
2025-04-22 13:24:29 -0500

    First iteration, pulling JSON response from AlphaVantage

--- 0_APItoCSV/main.py
+++ 0_APItoCSV/main.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+import requests
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=70H117QUS6AVTGV5'
+
+r = requests.get(url)
+
+data = r.json()
+
+print(data)
